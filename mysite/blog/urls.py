e965820e5e330bda102d550f1e88f3514108e86d
Narek3303 <margaryannarek056@gmail.com>
2024-01-30 23:13:24 +0400

    Blog

---
+++ mysite/blog/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from .feeds import LatestPostFeed
+
+
+app_name = 'blog'
+
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/',
+         views.post_share, name='post_share'),
+    path('<int:post_id>/comment/',
+        views.post_comment, name='post_comment'),
+    path('feed/', LatestPostFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
+
+]
